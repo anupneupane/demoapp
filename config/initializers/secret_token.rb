9cbd2585e9e192dda4baec40d91d19e7aa2034c4
Anup Neupane <anupbro@gmail.com>
2012-10-13 22:22:54 +0545

    Rename README.rdoc to README.md

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demoapp::Application.config.secret_token = 'c99c56dcda9211bc6a1f9df6adc5e3d18d5648ad4a60b4b9197dbf3bac1df89481763fe674c0bf5c2f9279eb97e68a00adb37ed371afa77ad86c39d0cab6f38b'
